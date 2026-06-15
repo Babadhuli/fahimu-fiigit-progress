0503a1f27ab8a246bd05ad75aad3c84c4e73c6dd
Babadhuli <compsykit@gmail.com>
2026-06-15 10:08:35 +0300

    Add files via upload

---
+++ Swapping.py
@@ -0,0 +1,8 @@
+a=int (input("Enter the first number: "))
+b=int (input("Enter the second number: "))
+temp=a
+a=b
+b=temp
+print("\n----Results after swapping are:----\n")
+print("First number: "+str(a))
+print("Second number: "+str(b))
